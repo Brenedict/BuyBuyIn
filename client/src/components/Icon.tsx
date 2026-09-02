81a6bf10833e4f7b47e6ed18e67e0083b12d494f
John Benedict Biñas <131650681+Brenedict@users.noreply.github.com>
2026-09-02 14:48:14 +0800

    feat: created text component and story for it. (#13)

---
+++ client/src/components/Icon.tsx
@@ -0,0 +1,42 @@
+// Types
+import {
+    type MaterialIcon,
+    type SizeVariant,
+    SizeClasses,
+    type ColorVariant,
+    ColorClasses,
+    type AlignVariant,
+    AlignClasses,
+} from "../types/common";
+
+export interface IconProps {
+    icon: MaterialIcon;
+    size: SizeVariant;
+    variant: ColorVariant;
+    align?: AlignVariant;
+    bg?: {
+        variant: ColorVariant;
+        type: "normal" | "circle";
+        padding: "small" | "big";
+    };
+}
+
+function Icon({ icon, size, variant, bg, align = "center" }: IconProps) {
+    const IconType = icon;
+
+    // Get background styles if it exists
+    const bgStyles = bg
+        ? `${ColorClasses[bg.variant].bg} ${bg.type === "normal" ? "rounded-xl" : "rounded-full"} ${bg.padding === "small" ? "p-2" : "p-3"}`
+        : "";
+
+    return (
+        <div className={` aspect-square flex w-fit ${AlignClasses[align]} items-center ${bgStyles}`}>
+            <IconType
+                className={`${ColorClasses[variant].text}  stroke-0 `}
+                style={{ fontSize: `var(--${SizeClasses[size]})` }}
+            />
+        </div>
+    );
+}
+
+export default Icon;
---
+++ client/src/types/common.ts
@@ -0,0 +1,94 @@
+import type { SvgIconTypeMap } from "@mui/material";
+import type { OverridableComponent } from "@mui/material/OverridableComponent";
+
+export type MaterialIcon = OverridableComponent<SvgIconTypeMap<object, "svg">> & {
+    muiName: string;
+};
+
+// ------------------------ FIXED Text Font Classes and Type ------------------------
+
+export const FontClasses = {
+    default: "font-sans-flex",
+    ligconsolata: "font-ligconsolata",
+} as const;
+
+export type FontVariant = keyof typeof FontClasses;
+
+// ------------------------ FIXED Text Size Classes and Type ------------------------
+
+// Size Classes (Text and SVG)
+export const SizeClasses = {
+    iconHero: "text-icon-hero",
+    larger: "text-larger",
+    large: "text-large",
+    bigger: "text-bigger",
+    big: "text-big",
+    medium: "text-medium",
+    normal: "text-normal",
+    description: "text-description",
+    small: "text-small-description",
+    smaller: "text-smaller-description",
+    smallest: "text-smallest-description",
+} as const;
+
+// Extracts: "giant" | "bigger"| "big" | "description"...
+export type SizeVariant = keyof typeof SizeClasses;
+
+// ------------------------ FIXED Text Weight Classes and Type ------------------------
+
+export const WeightClasses = {
+    black: "font-black",
+    bold: "font-bold",
+    medium: "font-medium",
+    regular: "font-regular",
+    light: "font-light",
+} as const;
+
+// Extracts: "bold" | "normal" | "thin"
+export type WeightVariant = keyof typeof WeightClasses;
+
+// ------------------------ FIXED Bg and Text Color Classes and Type ------------------------
+
+export const ColorClasses = {
+    black: { bg: "bg-black", text: "text-black" },
+    slate: { bg: "bg-slate", text: "text-slate" },
+    "slate-dark": { bg: "bg-slate-dark", text: "text-slate-dark" },
+    "slate-medium": { bg: "bg-slate-medium", text: "text-slate-medium" },
+    "slate-light": { bg: "bg-slate-light", text: "text-slate-light" },
+    brown: { bg: "bg-brown", text: "text-brown" },
+    maroon: { bg: "bg-maroon", text: "text-maroon" },
+    crimson: { bg: "bg-crimson", text: "text-crimson" },
+    "crimson-muted": { bg: "bg-crimson-muted", text: "text-crimson-muted" },
+    cream: { bg: "bg-cream", text: "text-cream" },
+    "cream-muted": { bg: "bg-cream-muted", text: "text-cream-muted" },
+} as const;
+
+// Extracts: "black" | "slate" | "slate-medium" | "crimson"
+export type ColorVariant = keyof typeof ColorClasses;
+
+// ------------------------ FIXED Text Alignment Classes and Type ------------------------
+
+export const AlignClasses = {
+    left: "text-left",
+    right: "text-right",
+    center: "text-center",
+} as const;
+
+// Extracts: "left", "right", "center"
+export type AlignVariant = keyof typeof AlignClasses;
+
+// ------------------------ FIXED Text SVG Position and Gap Classes and Type ------------------------
+export type svgPosition = "left" | "right"; // For position
+
+export const GapClasses = {
+    none: "gap-0",
+    small: "gap-1",
+    base: "gap-1.5",
+    medium: "gap-2",
+    big: "gap-3",
+    large: "gap-4",
+    extraLarge: "gap-6",
+} as const;
+
+// Extracts: "none", "small", "medium", "large", "extraLarge"
+export type GapVariant = keyof typeof GapClasses;

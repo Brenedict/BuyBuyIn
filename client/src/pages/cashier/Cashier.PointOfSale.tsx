1e005f9c906f5aa1d63a76e853f4240216a9a263
Siegfred-Augustine <rhomersiegfred.agustin254591@gmail.com>
2026-09-19 01:36:49 +0800

    feat:added calculator component and story

--- client/src/pages/cashier/Cashier.PointOfSale.tsx
+++ client/src/pages/cashier/Cashier.PointOfSale.tsx
@@ -1,15 +1,19 @@
 import { Card } from "../../components/Card";
 import { Text } from "../../components/Text";
 import { Button } from "../../components/Button";
+import { Calculator } from "../../components/Calculator";
 
 export function Cashier_PointOfSale() {
     return (
-        <Card>
-            <Card.Body className="flex flex-col gap-6">
-                <Text>Cashier Point of Sale</Text>
-                <Button>Button</Button>
-            </Card.Body>
-        </Card>
+        <div>
+            <Card>
+                <Card.Body className="flex flex-col gap-6">
+                    <Text>Cashier Point of Sale</Text>
+                    <Button>Button</Button>
+                </Card.Body>
+            </Card>
+            <Calculator />
+        </div>
     );
 }
 

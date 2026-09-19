7ae798af278ed97c8d398902ec5cf46033c7fac9
Siegfred-Augustine <rhomersiegfred.agustin254591@gmail.com>
2026-09-19 23:21:24 +0800

    feat: added page structure

--- client/src/pages/cashier/Cashier.PointOfSale.tsx
+++ client/src/pages/cashier/Cashier.PointOfSale.tsx
@@ -2,16 +2,12 @@ import { Card } from "../../components/Card";
 import { Text } from "../../components/Text";
 import { Button } from "../../components/Button";
 import { Calculator } from "../../components/Calculator";
+import { SearchInput } from "../../components/input/SearchInputs";
 
 export function Cashier_PointOfSale() {
     return (
         <div>
-            <Card>
-                <Card.Body className="flex flex-col gap-6">
-                    <Text>Cashier Point of Sale</Text>
-                    <Button>Button</Button>
-                </Card.Body>
-            </Card>
+            <section></section>
             <Calculator />
         </div>
     );
